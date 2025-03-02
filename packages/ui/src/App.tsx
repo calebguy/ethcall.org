69f951f37b6ecbf65c7faa0add3f932263b99d79
caleb <caleb@syndicate.io>
2025-03-01 23:57:35 -0700

    〠

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
 	return (
-		<div className="flex flex-col gap-3">
-			<div>eth.call</div>
+		<div className="text-6xl md:text-8xl text-orange p-6 text-center md:text-left">
+			<div className="underline">
+				<span className="font-bold">ethcall</span>
+				<span className="text-yellow">.org</span>
+			</div>
+			{/* <div className="w-full border-t border-white my-8" /> */}
 		</div>
 	);
 }
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				orange: "#e05333",
+				background: "#1e1f21",
+				yellow: "#ecb366",
+				blue: "#4c7bc2",
+			},
+		},
 	},
 	plugins: [],
 };
