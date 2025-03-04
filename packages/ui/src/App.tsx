96e601c998e037774da4b5ff5f6a05944b21cacd
caleb <caleb@syndicate.io>
2025-03-04 01:45:57 -0600

    up

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,11 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import classNames from "classnames";
-import { useState } from "react";
-import { IoMdClose } from "react-icons/io";
 import { getRequests } from "./api";
 
 function App() {
-	const [show, setShow] = useState(true);
 	const { data, isLoading } = useQuery({
 		queryKey: ["requests"],
 		queryFn: () => getRequests(),
@@ -15,8 +12,7 @@ function App() {
 	return (
 		<div
 			className={classNames(
-				"text-6xl md:text-8xl text-primary px-6 pt-6 text-center md:text-left min-h-svh relative",
-				{ "pb-36": show, "pb-6": !show },
+				"text-6xl md:text-8xl text-primary p-6 text-center md:text-left min-h-svh relative",
 			)}
 		>
 			<div className="">ethcall.org</div>
@@ -41,24 +37,13 @@ function App() {
 					})}
 				{isLoading && <div>Loading...</div>}
 			</div>
-			{show && (
-				<div className="fixed bottom-0 left-0 w-full p-4 flex">
-					<div className="grow mt-8 backdrop-blur-2xl text-4xl border border-primary rounded-md p-4 border-dashed flex items-center justify-center">
-						<button
-							type="button"
-							onClick={() => {
-								setShow(false);
-							}}
-							className="hover:bg-primary hover:text-background transition-all duration-100 rounded-sm text-4xl"
-						>
-							<IoMdClose />
-						</button>
-						<code className="text-primary break-all text-2xl md:text-4xl grow text-center inline-block">
-							<span>curl https://ethcall.org/:chainId/:address/:fnSig</span>
-						</code>
-					</div>
+			<div className="fixed bottom-0 left-0 w-full p-4 flex">
+				<div className="grow mt-8 backdrop-blur-2xl text-4xl border border-primary rounded-md p-4 border-dashed flex items-center justify-center">
+					<code className="text-primary break-all text-2xl md:text-4xl grow text-center inline-block">
+						<span>curl https://ethcall.org/:chainId/:address/:fnSig</span>
+					</code>
 				</div>
-			)}
+			</div>
 		</div>
 	);
 }
