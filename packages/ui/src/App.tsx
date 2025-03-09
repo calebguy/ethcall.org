cb01a4ceca98a270edd3ce4d935ba971a9741ef3
caleb <caleb@syndicate.io>
2025-03-08 23:31:34 -0600

    pretty

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -33,12 +33,12 @@ function App() {
 								target="_blank"
 								rel="noopener noreferrer"
 								href={`${import.meta.env.VITE_API_BASE_URL}${req.path}`}
-								className="leading-8 md:leading-10 text-2xl md:text-4xl group relative w-full p-6 md:p-8 break-all"
+								className="leading-8 md:leading-10 text-2xl md:text-4xl group relative w-full p-6 md:p-8 break-all hover:bg-magenta/15 rounded-xl"
 							>
-								<RxCornerTopLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute top-0 left-0 text-dark-brown" />
-								<RxCornerTopRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute top-0 right-0 text-dark-brown" />
-								<RxCornerBottomLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute bottom-0 left-0 text-dark-brown" />
-								<RxCornerBottomRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute bottom-0 right-0 text-dark-brown" />
+								<RxCornerTopLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-2 -left-2 text-dark-brown" />
+								<RxCornerTopRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-2 -right-2 text-dark-brown" />
+								<RxCornerBottomLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-2 -left-2 text-dark-brown" />
+								<RxCornerBottomRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-2 -right-2 text-dark-brown" />
 								<span className="group-hover:opacity-0 inline-block">
 									<span className="font-light text-dark-brown">/{chainId}</span>
 									<span className="font-light text-dark-brown">
@@ -52,8 +52,11 @@ function App() {
 										</span>
 									)}
 								</span>
-								<span className="hidden break-all text-magenta absolute top-0 left-8 mx-auto group-hover:flex items-center justify-start w-full h-full text-4xl overflow-auto tracking-tighter">
+								<span className="hidden break-all text-magenta absolute top-0 left-8 mx-auto group-hover:flex items-center justify-start w-full h-full  md:text-4xl text-overflow-ellipsis overflow-hidden tracking-tighter">
 									<span>{req.result}</span>
+									{/* <span className="inline-flex grow">
+										<MdArrowOutward className="w-10 h-10" />
+									</span> */}
 								</span>
 							</a>
 						);
