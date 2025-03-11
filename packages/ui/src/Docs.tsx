be67759632de1402a360f8d16068d2024849b637
caleb <caleb@syndicate.io>
2025-03-11 12:08:17 -0500

    up

---
+++ packages/ui/src/Docs.tsx
@@ -0,0 +1,7 @@
+export function Docs() {
+	return (
+		<div className="p-6 text-center md:text-left">
+			<h1>Docs</h1>
+		</div>
+	);
+}
---
+++ packages/ui/src/Home.tsx
@@ -0,0 +1,74 @@
+import { useQuery } from "@tanstack/react-query";
+import classNames from "classnames";
+import {
+	RxCornerBottomLeft,
+	RxCornerBottomRight,
+	RxCornerTopLeft,
+	RxCornerTopRight,
+} from "react-icons/rx";
+import { getRequests } from "./api";
+import { abbreviateAddress, splitFunctionSignature } from "./utils";
+
+export function Home() {
+	const { data, isLoading } = useQuery({
+		queryKey: ["requests"],
+		queryFn: () => getRequests(),
+		refetchInterval: 10000,
+	});
+
+	return (
+		<div
+			className={classNames(
+				"p-6 text-center md:text-left min-h-dvw tracking-normal flex flex-col",
+			)}
+		>
+			<div className="flex flex-col gap-4 items-start grow px-2 py-2">
+				{!isLoading &&
+					data?.requests?.map((req) => {
+						const [, chainId, address, fnSig, ...args] = req.path.split("/");
+						const { signature, returnType } = splitFunctionSignature(fnSig);
+						return (
+							<a
+								key={req.id}
+								target="_blank"
+								rel="noopener noreferrer"
+								href={`${import.meta.env.VITE_API_BASE_URL}${req.path}`}
+								className="leading-8 md:leading-10 text-2xl md:text-4xl group relative w-full p-6 md:p-8 break-all hover:bg-magenta/15 rounded-[10px] border border-transparent hover:border-magenta transition-all duration-100"
+							>
+								<RxCornerTopLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-[10px] -left-[10px] text-dark-brown" />
+								<RxCornerTopRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-[10px] -right-[10px] text-dark-brown" />
+								<RxCornerBottomLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-[10px] -left-[10px] text-dark-brown" />
+								<RxCornerBottomRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-[10px] -right-[10px] text-dark-brown" />
+								<span className="group-hover:opacity-0 inline-block">
+									<span className="font-light text-dark-brown">/{chainId}</span>
+									<span className="font-light text-dark-brown">
+										/{abbreviateAddress(address)}
+									</span>
+									<span className="font-bold text-orange">/{signature}</span>
+									<span className="font-bold text-green">{returnType}</span>
+									{args.length > 0 && (
+										<span className="text-white font-bold">
+											/{args.join(",")}
+										</span>
+									)}
+								</span>
+								<span className="hidden break-all text-magenta absolute top-0 left-8 group-hover:flex items-center justify-start w-full h-full  md:text-4xl text-overflow-ellipsis overflow-hidden tracking-tighter">
+									<span className="text-wrap">{req.result}</span>
+								</span>
+							</a>
+						);
+					})}
+				<div className="text-dark-brown text-center w-full">
+					~ eth_call me ~
+				</div>
+				{isLoading && <div>Loading...</div>}
+			</div>
+			<div className="fixed min-w-dvw bottom-0 left-0 text-center text-dark-brown backdrop-blur-[1px] p-4 shadow-dark-brown/20">
+				<span className="text-4xl md:text-7xl font-bold">ethcall.org</span>
+				<span className="text-2xl md:text-4xl font-bold break-all">
+					/:chainId/:address/:fnSig/:arg1,:arg2...
+				</span>
+			</div>
+		</div>
+	);
+}
