470f0e09c1ee0e39f43257f3bcd0e0fad99c83d5
caleb <caleb@syndicate.io>
2025-03-11 22:55:28 -0500

    docs

--- packages/ui/src/Docs.tsx
+++ packages/ui/src/Docs.tsx
@@ -1,30 +1,70 @@
+import {
+	RxCornerBottomLeft,
+	RxCornerBottomRight,
+	RxCornerTopLeft,
+	RxCornerTopRight,
+} from "react-icons/rx";
+
 export function Docs() {
 	return (
-		<div className="p-6 text-center md:text-left text-2xl md:text-4xl flex flex-col gap-14 min-h-dvh">
-			<section>
-				<div>
-					<span className="text-dark-brown">Base URL:</span>{" "}
-					<span className="text-dark-brown">https://ethcall.org</span>
+		<div className="p-6 text-2xl md:text-4xl flex flex-col gap-14 min-h-dvh">
+			<section className="text-magenta relative p-6 md:p-8">
+				<RxCornerTopLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-[10px] -left-[10px] text-dark-brown" />
+				<RxCornerTopRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-[10px] -right-[10px] text-dark-brown" />
+				<RxCornerBottomLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-[10px] -left-[10px] text-dark-brown" />
+				<RxCornerBottomRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-[10px] -right-[10px] text-dark-brown" />
+				<div className="flex flex-col items-center md:items-start gap-2 break-words overflow-auto text-wrap">
+					<span className="bg-dark-brown p-2 rounded-xl text-white">GET</span>
+					<div
+						style={{ overflowWrap: "anywhere" }}
+						className="overflow-auto text-wrap break-words"
+					>
+						<span className="text-dark-brown">
+							https://ethcall.org/:chainId/:address
+						</span>
+						<span className="text-orange">/:fnSig</span>
+						<span className="text-orange">{"("}</span>
+						<span className="text-orange/60">arg1,arg2,...</span>
+						<span className="text-orange">{")"}</span>
+						<span className="text-green">(returnType...)</span>
+						<span className="text-white">/:arg1,:arg2...</span>
+					</div>
+				</div>
+				<div className="mt-14 flex flex-col items-center md:items-start gap-4">
+					<span className="bg-dark-brown p-2 rounded-xl italic">RETURNS</span>
+					<div className="text-white italic">
+						<span>{"{"}</span>
+						<span>result:</span>
+						<span>"string | object"</span>
+						<span>{"}"}</span>
+					</div>
 				</div>
 			</section>
-			<section>
-				<div className="flex items-center gap-2">
-					<span>GET</span>
-					<div>/:chainId/:address/:fnSig/:arg1,:arg2...</div>
+			<section className="relative p-6 md:p-8">
+				<RxCornerTopLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-[10px] -left-[10px] text-dark-brown" />
+				<RxCornerTopRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -top-[10px] -right-[10px] text-dark-brown" />
+				<RxCornerBottomLeft className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-[10px] -left-[10px] text-dark-brown" />
+				<RxCornerBottomRight className="group-hover:text-magenta md:w-10 md:h-10 w-8 h-8 absolute -bottom-[10px] -right-[10px] text-dark-brown" />
+				<div className="flex flex-col items-center md:items-start gap-2 break-words overflow-auto text-wrap">
+					<span className="bg-dark-brown text-white p-2 rounded-xl">GET</span>
+					<div
+						style={{ overflowWrap: "anywhere" }}
+						className="overflow-auto text-wrap break-words"
+					>
+						<span className="text-dark-brown">https://ethcall.org</span>
+						<span className="text-orange">/chains</span>
+					</div>
 				</div>
-				<div className="ml-10">
-					{JSON.stringify({ result: "string | object" })}
-				</div>
-			</section>
-			<section>
-				<div className="flex items-center gap-2">
-					<span>GET</span>
-					<div>/chains</div>
-				</div>
-				<div className="ml-10">
-					Array{"<"}
-					{JSON.stringify({ chainId: "number", chainName: "string" })}
-					{">"}
+				<div className="mt-14 flex flex-col items-center md:items-start gap-4">
+					<span className="bg-dark-brown p-2 rounded-xl text-magenta italic">
+						RETURNS
+					</span>
+					<div
+						style={{ overflowWrap: "anywhere" }}
+						className="text-white italic overflow-auto text-wrap break-words"
+					>
+						{JSON.stringify({ chainId: "number", chainName: "string" })}[]
+					</div>
 				</div>
 			</section>
 		</div>
