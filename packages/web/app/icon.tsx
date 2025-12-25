ba1edbfa9b37fcca072e45a5b5c9e7e02d5e9bfb
caleb <caleb@syndicate.io>
2025-12-25 09:51:34 -0500

    favi

---
+++ packages/web/app/icon.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from "next/og";
+
+export const size = {
+	width: 32,
+	height: 32,
+};
+export const contentType = "image/png";
+
+export default function Icon() {
+	return new ImageResponse(
+		(
+			<div
+				style={{
+					width: "100%",
+					height: "100%",
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					backgroundColor: "transparent",
+				}}
+			>
+				<div
+					style={{
+						width: "100%",
+						height: "100%",
+						backgroundColor: "#A37D65",
+						borderRadius: 6,
+					}}
+				/>
+			</div>
+		),
+		{
+			...size,
+		}
+	);
+}
